1b95b36ca3451bf955fa09f2508e475032dedb2b
EjnarRaidriar <emanuil.cucos25@gmail.com>
2025-03-19 11:03:09 +0200

    implement search

--- go2web.go
+++ go2web.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"os"
 	"strings"
 )
@@ -25,6 +29,7 @@ func main() {
 			fmt.Println("Error:\n\t-s expects at least one argument")
 		} else {
 			fmt.Printf("Searching term: %s\n", strings.Join(args[1:], " "))
+			search(args[1:])
 		}
 	}
 	if args[0] == "-h" {
@@ -35,4 +40,53 @@ func main() {
 		fmt.Println("Show this help")
 		fmt.Println("\t-h")
 	}
+}
+
+func toGoogleSearch(search []string) string {
+	var searchBuilder strings.Builder
+	for _, term := range search {
+		if strings.Contains(term, "+") {
+			var sb strings.Builder
+			for _, char := range term {
+				if char == '+' {
+					sb.WriteString("%2B")
+				} else {
+					sb.WriteRune(char)
+				}
+			}
+			searchBuilder.WriteString(sb.String())
+			searchBuilder.WriteRune('+')
+		} else {
+			searchBuilder.WriteString(term)
+			searchBuilder.WriteRune('+')
+		}
+	}
+	result := searchBuilder.String()
+	return result[:len(result)-1]
+}
+
+func search(arguments []string) {
+	host := "www.google.com"
+	path := "/search?q=" + toGoogleSearch(arguments)
+
+	addr, err := net.ResolveTCPAddr("tcp", host+":80")
+	if err != nil {
+		log.Fatal("could not resolve tcp address:", err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal("could not dial:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Sending request to: " + host + path)
+	httpReq := "GET " + path + " HTTP/1.0\r\n"
+	httpReq += "\r\n"
+	fmt.Fprintf(conn, "%s", httpReq)
+
+	var buf bytes.Buffer
+	io.Copy(&buf, conn)
+	fmt.Printf("got this response:\n%s", buf.Bytes())
 }
